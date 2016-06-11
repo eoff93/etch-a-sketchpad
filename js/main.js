92bec85bce48602cff56fd2bf78aa2eb0f2c54c5
eoff93 <bane.dev101@gmail.com>
2016-06-11 13:54:46 +0200

    Change background of a box to random color on hover

--- js/main.js
+++ js/main.js
@@ -18,4 +18,18 @@ function appendGrid(grid) {
   $('.container').append(grid);
 }
 
+// returns a random color in hex format
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'.split('');
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 appendGrid(generateGrid(16));
+
+$('.box').hover(function setRandomBackground() {
+  $(this).css('background', getRandomColor());
+});
