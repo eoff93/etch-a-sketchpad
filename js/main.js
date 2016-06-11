54ebf1f8535a82018330be183d4d253969f14069
eoff93 <bane.dev101@gmail.com>
2016-06-11 13:48:09 +0200

    Generate a test grid using javascript and jquery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+// generate a grid of boxes based on size entered
+function generateGrid(size) {
+  let grid = '';
+  for (let i = 0; i < size; i++) {
+    grid += '<section class="row">';
+
+    for (let j = 0; j < size; j++) {
+      grid += '<div class="box"></div>';
+    }
+    grid += '</section>';
+  }
+  return grid;
+}
+
+// append generated grid to container
+function appendGrid(grid) {
+  $('.container').append(grid);
+}
+
+appendGrid(generateGrid(16));
